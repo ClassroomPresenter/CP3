4d94f71706b732e7dc536b42581945f6bf115722
Fred Videon <fred@cs.washington.edu>
2014-07-09 15:15:27 -0700

    Increment rev number to 2240

--- Presenter/AssemblyInfo.cs
+++ Presenter/AssemblyInfo.cs
@@ -5,14 +5,14 @@ using System.Runtime.InteropServices;
 using System.Security.Permissions;
 
 [assembly: AssemblyTitle("Classroom Presenter 3")]
-[assembly: AssemblyDescription("Revision 2239 ; 6/18/2014")]
+[assembly: AssemblyDescription("Revision 2240 ; 7/9/2014")]
 // [assembly: AssemblyConfiguration(...)]
 [assembly: AssemblyCompany("University of Washington, Department of Computer Science and Engineering")]
 [assembly: AssemblyProduct("Classroom Presenter 3")]
 [assembly: AssemblyCopyright("(c) 2006.  All Rights Reserved.  See http://www.cs.washington.edu/education/dl/presenter/ for details.")]
 // [assembly: AssemblyTrademark(...)]
 // [assembly: AssemblyCulture(...)]	
-[assembly: AssemblyVersion("3.1.2239.*")]
+[assembly: AssemblyVersion("3.1.2240.*")]
 [assembly: AssemblyDelaySign(false)]
 // [assembly: AssemblyKeyFile(...)]
 // [assembly: AssemblyKeyName(...)]
--- UW.ClassroomPresenter/AssemblyInfo.cs
+++ UW.ClassroomPresenter/AssemblyInfo.cs
@@ -4,14 +4,14 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Classroom Presenter 3: Libraries")]
-[assembly: AssemblyDescription("Revision 2239")]
+[assembly: AssemblyDescription("Revision 2240")]
 // [assembly: AssemblyConfiguration(...)]
 [assembly: AssemblyCompany("University of Washington, Department of Computer Science and Engineering")]
 [assembly: AssemblyProduct("Classroom Presenter 3")]
 [assembly: AssemblyCopyright("(c) 2006.  All Rights Reserved.  See http://www.cs.washington.edu/education/dl/presenter/ for details.")]
 // [assembly: AssemblyTrademark(...)]
 // [assembly: AssemblyCulture(...)]	
-[assembly: AssemblyVersion("3.0.2239.*")]
+[assembly: AssemblyVersion("3.0.2240.*")]
 [assembly: AssemblyDelaySign(false)]
 // [assembly: AssemblyKeyFile(...)]
 // [assembly: AssemblyKeyName(...)]
